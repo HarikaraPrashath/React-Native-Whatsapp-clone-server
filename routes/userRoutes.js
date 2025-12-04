28dfab7dcffe3ac5c3b9742f042ee36ac63acbe2
HarikaraPrashath <prashathhari2002@gmail.com>
2025-12-04 11:09:56 +0530

    create user with image uploading

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,20 @@
-import express from "express";
+import express, { response } from "express";
 import User from "../models/user.js";
+import multer from "multer";
+
 
 const router = express.Router();
+
+//setup multer for image upload
+const storage = multer.diskStorage({
+  destination:"uploads/",
+  filename:(req,file,cb)=>{
+    cb(null,`${Date.now()}-${file.originalname}`)
+  }
+})
+
+const upload = multer({storage})
+
 
 //Get user by mobile
 router.get('/:phone', async (req, res) => {
@@ -17,6 +30,24 @@ router.get('/:phone', async (req, res) => {
   }
 });
 
+//create User with Image upload API
+router.post('/',upload.single("profileImage"), async (req, res) => {
+  const {phone, name} = req.body;
+  try {
+    let user = await User.findOne({ phone });
+    if (user) {
+      return res.status(200).json({ message: "User already exists" });
+    }
+
+    const profileImage = req.file ? `/uploads/${req.file.filename}` : null;
+    user = new User({ phone, name, profileImage })
+    await user.save();
+
+    res.status(201).json({ message: "User created successfully", user });
+  } catch (error) {
+    res.status(500).json({ message: "Server Error : ", error: error.message });
+  }
+})
 
 
 export default router;
