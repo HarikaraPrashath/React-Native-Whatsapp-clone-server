887f4ab581fbcdbc743eed7ab087013cb6d05e10
HarikaraPrashath <prashathhari2002@gmail.com>
2025-12-04 10:41:53 +0530

    API_testing done

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,17 +4,19 @@ import User from "../models/user.js";
 const router = express.Router();
 
 //Get user by mobile
-router.get('/:phone/', async (req, res) => {
-    try {
-        const user = await User.findOne({ phone: req.params.phone })
+router.get('/:phone', async (req, res) => {
+  try {
+    const user = await User.findOne({ phone: req.params.phone })
 
-        if (!user) {
-            return res.status(404).json({ message: "User not found" })
-        }
-        res.json(user)
-    } catch (error) {
-        res.status(500).json({message:"Server Error : ", error:error.message});
+    if (!user) {
+      return res.status(404).json({ message: "User not found" })
     }
+    res.json(user)
+  } catch (error) {
+    res.status(500).json({ message: "Server Error : ", error: error.message });
+  }
 });
+
+
 
 export default router;
--- server.js
+++ server.js
@@ -3,24 +3,26 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js"
 
+
 dotenv.config();
 const app = express();
 app.use(express.json());
 
 
+const PORT = process.env.PORT || 5000;
 
 //Database connection
-mongoose.connect(process.env.REACT_NATIVE_APP_MONGO_URL)
-.then(() => { console.log("Connected to MongoDB") })
-.catch((err) => { console.log("Error connecting to MongoDB: ", err.message) });
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => { console.log("Connected to MongoDB") })
+  .catch((err) => { console.log("Error connecting to MongoDB: ", err.message) });
 
 
 app.get('/', (req, res) => {
-    res.send("Hello World");
+  res.send("Hello World");
 })
 
-app.use('/api//users',userRoutes)
+app.use('/api/users', userRoutes)
 
-app.listen(5000, () => {
-    console.log("Server is running on port 5000");
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
