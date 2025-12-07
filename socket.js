e5ed9002a5e912f73b02ed4765d48f0eae3a91c4
HarikaraPrashath <prashathhari2002@gmail.com>
2025-12-07 20:10:07 +0530

    Conversation model and schema was created

--- socket.js
+++ socket.js
@@ -1,24 +1,51 @@
-export default function registerSocketHandlers(io){
+import Conversation from "./models/Conversation.js";
+
+export default function registerSocketHandlers(io) {
     console.log("socket handlers called")
-    io.on("connection",(socket)=>{
-       const userId =  socket.handshake.auth.userId || socket.handshake.query.userId;
-    
-       console.log("Socket connected",socket.id)
+    io.on("connection", (socket) => {
+        const userId = socket.handshake.auth.userId || socket.handshake.query.userId;
 
-       if(userId){
-        socket.join(userId);//Personal room join
-        console.log(`user ${userId} join a personal room`)
-        
-       }
+        console.log("Socket connected", socket.id)
 
-       socket.on("join",(otherUserId)=>{
-        socket.join(otherUserId);
-        console.log(`user ${userId} with ${otherUserId} join a chat`)
-       })
-       socket.on("send-message",(data)=>{
-        const {otherUserId,message}= data;
+        if (userId) {
+            socket.join(userId);//Personal room join
+            console.log(`user ${userId} join a personal room`)
 
-        socket.to(otherUserId).emit("receive-message",message)
-       })
+        }
+
+        socket.on("join", (otherUserId) => {
+            socket.join(otherUserId);
+            console.log(`user ${userId} with ${otherUserId} join a chat`)
+        })
+        socket.on("send-message", async (data) => {
+            const { otherUserId, text } = data;
+
+            try {
+                //find or create conversation
+                let conversation = await Conversation.findOne({
+                    participants: { $all: [userId, otherUserId] }
+                }).populate("participants")
+
+                let isNew = false
+                if (!conversation) {
+                    isNew=true
+                    conversation = new Conversation({
+                        participants: [userId, otherUserId]
+                    })
+                }
+
+                await conversation.save()
+                socket.to(otherUserId).emit("receive-message", {
+                    text,
+                    conversation,
+                    isNew
+                })
+
+            } catch (error) {
+
+            }
+
+
+        })
     })
 }
