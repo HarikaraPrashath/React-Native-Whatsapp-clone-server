69bfb254770ab7926cfad07cb77bb3e22841acc8
HarikaraPrashath <prashathhari2002@gmail.com>
2025-12-08 11:50:25 +0530

    Message schema is working and save details

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import User from "../models/user.js";
+import User from "../models/User.js";
 import multer from "multer";
 import fs from "fs";
 import path from "path";
