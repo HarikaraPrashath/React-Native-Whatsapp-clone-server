1733ee678d77152bd6171329d4b07448fe4a548c
HarikaraPrashath <prashathhari2002@gmail.com>
2025-12-11 21:28:33 +0530

    conversation schema changed

--- models/Conversation.js
+++ models/Conversation.js
@@ -1,19 +1,20 @@
 import mongoose from "mongoose";
 
 const conversationSchema = new mongoose.Schema({
-    participants:{
-        type:[{type:mongoose.Schema.Types.ObjectId, ref:"User"}],
-        validate:[arr => arr.length == 2,"A conversation must have 2 participants"]
-    }, 
-    lastMessage:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"Message"
+    participants: {
+        type: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+        validate: [arr => arr.length == 2, "A conversation must have 2 participants"]
     },
-    unreadCounts:{
-        type:Map,
-        of:Number,
-        default:{},
-    }
-})
+    lastMessage: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Message"
+    },
+    unreadCounts: {
+        type: Map,
+        of: Number,
+        default: {},
+    },
 
-export default mongoose.model("Conversation",conversationSchema)
+}, { timestamps: true })
+
+export default mongoose.model("Conversation", conversationSchema)
